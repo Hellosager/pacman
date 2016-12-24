225f9a8ba786e860e16d4f475a8635553b669972
Sebastian Beck <Sebastian Beck>
2016-12-24 23:08:41 +0100

    fixed movement behavior of inky where he could target walls and out of bounds tiles

--- src/Pacman/creatures/Inky.java
+++ src/Pacman/creatures/Inky.java
@@ -5,7 +5,6 @@ import java.awt.image.BufferedImage;
 
 import Pacman.level.Level;
 import Pacman.level.pathfinder.Dijkstra;
-import Pacman.level.pathfinder.Knoten;
 import Pacman.tiles.Tile;
 
 public class Inky extends Ghost{
@@ -42,8 +41,30 @@ public class Inky extends Ghost{
 	private Point getDestinationNode() {
 		Point twoBeforePacman = getXNodesInFrontOfPacman(2); // zwei for Pacman
 		Ghost blinky = level.getGhosts()[0]; // Blinky
-		Point vektor = new Point(twoBeforePacman.x - blinky.getX(), twoBeforePacman.y - blinky.getY());
-		return new Point(vektor.x*2, vektor.y*2);
+		int vektorX = (twoBeforePacman.x - blinky.getX())*2;
+		int vektorY = (twoBeforePacman.y - blinky.getY())*2;
+		while(!pointIsValid(blinky.getX() + vektorX, blinky.getY() + vektorY)){
+			if(Math.abs(vektorX) > Math.abs(vektorY)){	// wenn x koord größer als y ist
+				if(vektorX < 0)							// wenn vektorX negativ, dann addieren
+					vektorX++;
+				else									// sonst subtrahieren
+					vektorX--;
+			}else{										// Ansonsten ist y größer
+				if(vektorY < 0 )						// wenn y negativ, dann addieren +1
+					vektorY++;
+				else									// Ansonsten -1
+					vektorY--;
+			}
+		}
+		// Vektor von Blinky zum legalen doppelten
+		return new Point(blinky.getX() + vektorX, blinky.getY() + vektorY);	// das letztendliche Zielfeld
+		
 	}
+	private boolean pointIsValid(int x, int y){
+		return	x > 0 && x < level.getWidth() &&	// und auf x in bounds
+				y > 0 && y < level.getHeight() &&	// und auf y in bounds
+				level.getTileMap()[x][y] != Tile.WALL;	// Vektor valid if no wall
+	}
+	
 	
 }
